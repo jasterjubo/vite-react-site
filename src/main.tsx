548d970fe1fa832500fd4f4394e6baf59b53df58
haowenchang <jaster@JasterdeMacBook-Pro.local>
2024-03-29 14:29:00 +0800

    react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
+// import App from './App.tsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Landing Page</div>
+  },
+  {
+    path: '/test',
+    element: <div>Test Page</div>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
